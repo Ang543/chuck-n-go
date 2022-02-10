fab44c93b9d37cddfe25a13832e19f34cf7e180f
Hannah Bush <heb.rutgers@gmail.com>
2022-02-09 20:19:24 -0500

    js pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-let apiMq = "XZSAH1ikLn8zpZjUGzEFqnthzNyKVjIY"
-let apiOpenTrip = "5ae2e3f221c38a28845f05b604ac0aedf17596d60d10c95865fde816"
+let apiMq = "XZSAH1ikLn8zpZjUGzEFqnthzNyKVjIY";
+let apiOpenTrip = "5ae2e3f221c38a28845f05b604ac0aedf17596d60d10c95865fde816";
 
+//get chuckjoke from api and display for user for every new trip
+
+//capture user input for tansport mode
+
+//capture user input for to and from
+
+//capture user input for things they want to see
+
+//call Mq to get route using user inputs
+
+//call opentrip to get attractions along route
+
+//allow user to save a trip
