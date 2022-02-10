2244150536df12ef85c6bd166d577ee4bae48545
Hannah Bush <heb.rutgers@gmail.com>
2022-02-09 20:48:43 -0500

    trying to capture radio data

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,10 @@ let apiOpenTrip = "5ae2e3f221c38a28845f05b604ac0aedf17596d60d10c95865fde816";
 //get chuckjoke from api and display for user for every new trip
 
 //capture user input for tansport mode
-
+$("#mode").on("click", function() {
+  var mode = document.querySelector('input[name="modes"]:checked').value;
+  console.log(mode);
+});
 //capture user input for to and from
 
 //capture user input for things they want to see
