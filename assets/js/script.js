5c0f3baea2a2cf2d3f87875a294c780eeea07bf9
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-02-10 22:50:58 -0500

    Map Api tested

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 let apiMq = "XZSAH1ikLn8zpZjUGzEFqnthzNyKVjIY"
-let apiOpenTrip = "5ae2e3f221c38a28845f05b604ac0aedf17596d60d10c95865fde816"
+let apiOpenKey = "5ae2e3f221c38a28845f05b604ac0aedf17596d60d10c95865fde816"
 
+var getMapObject = function () {
+  var response = ("http://api.opentripmap.com/0.1/en/places/bbox?lon_min=38.364285&lat_min=59.855685&lon_max=38.372809&lat_max=59.859052&kinds=museums&format=geojson&apikey=" + apiOpenKey);
+  fetch(response).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+};
+
+
+
+getMapObject();
