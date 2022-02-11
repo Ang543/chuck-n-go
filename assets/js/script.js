15fb657d1e2308d0aab889596044576b85d22d80
techman <dmoloney5@gmail.com>
2022-02-11 15:18:48 -0500

    finished joke api and adding to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const jokeEl = document.getElementById('jokeP')
+const jokeEl = document.getElementById('.jokeP')
 let apiMq = "XZSAH1ikLn8zpZjUGzEFqnthzNyKVjIY";
 let apiOpenTrip = "5ae2e3f221c38a28845f05b604ac0aedf17596d60d10c95865fde816";
 
@@ -12,18 +12,33 @@ function jokeData() {
       }
       return response.json();
     })
-    .then(data => {
+    .then(data => { 
       console.log(data.value)
-      document.querySelector("#cnjoke")
-        
+      document.getElementById("jokeP").append(data.value)     
     })
     .catch(error => {
       console.log(error)
     });
   }
 
+  showJoke = (dataObjects, div) => {
+    const dataDiv = document.querySelector(div)
+    dataObjects.forEach(dataObject => {
+        const dataElement = document.createElement('p')
+        dataElement.innerText= `Name: ${dataObject.name}`
+        dataDiv.append(dataElement)
+    })
+
+}
   jokeData()
 
+//capture user input for tansport mode
+//hannah
+
+$("#mode").on("click", function() {
+  var mode = document.querySelector('input[name="modes"]:checked').value;
+  console.log(mode);
+});
 //capture user input for to and from
 //angelo
 
