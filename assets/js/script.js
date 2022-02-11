3728f40ee963ed4e139d59f20e33d2f6f54b8f2a
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-02-11 10:40:32 -0500

    set vinput vaiables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 let apiMq = "XZSAH1ikLn8zpZjUGzEFqnthzNyKVjIY"
 let apiOpenKey = "5ae2e3f221c38a28845f05b604ac0aedf17596d60d10c95865fde816"
 
+var attractEl = document.querySelector("#attract");
+var scenicEL = document.querySelector("#scenic")
+var hotelEl = document.querySelector("#hotel")
+var dineEL = document.querySelector("#dine")
+var entEl = document.querySelector("#ent")
+
 var getMapObject = function () {
   var response = ("http://api.opentripmap.com/0.1/en/places/bbox?lon_min=38.364285&lat_min=59.855685&lon_max=38.372809&lat_max=59.859052&kinds=museums&format=geojson&apikey=" + apiOpenKey);
   fetch(response).then(function (response) {
@@ -13,3 +19,6 @@ var getMapObject = function () {
 
 
 getMapObject();
+
+//user location = long-min + Lat-min
+//user destination = long-max + lat-max
