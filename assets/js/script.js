00488b4d0c33e6fc21582eeee37cdb7142c4e8e8
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-02-11 21:14:49 -0500

    get user info in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 let apiMq = "XZSAH1ikLn8zpZjUGzEFqnthzNyKVjIY"
 let apiOpenKey = "5ae2e3f221c38a28845f05b604ac0aedf17596d60d10c95865fde816"
 
-var attractEl = document.querySelector("#attract");
-var scenicEL = document.querySelector("#scenic")
-var hotelEl = document.querySelector("#hotel")
-var dineEL = document.querySelector("#dine")
-var entEl = document.querySelector("#ent")
+var placesEL = ["accomodations","amusement","tourist_facilities","natural","adult"];
+var loclatMin = "";
+var loclongMin = "";
+var desLatMax = "";
+var desLongMax = "";
 
+
+
+var attractEl = document.querySelector("#attract");
+var scenicEL = document.querySelector("#scenic");
+var hotelEl = document.querySelector("#hotel");
+var dineEL = document.querySelector("#dine");
+var entEl = document.querySelector("#ent");
+
+
+//this function gets attractions data based on radius
 var getMapObject = function () {
-  var response = ("http://api.opentripmap.com/0.1/en/places/bbox?lon_min=38.364285&lat_min=59.855685&lon_max=38.372809&lat_max=59.859052&kinds=museums&format=geojson&apikey=" + apiOpenKey);
+  var response = ("http://api.opentripmap.com/0.1/en/places/bbox?lon_min=30.364285&lat_min=49.855685&lon_max=38.372809&lat_max=59.859052&kinds=interesting_places&format=geojson&apikey=" + apiOpenKey);
   fetch(response).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
@@ -20,5 +30,14 @@ var getMapObject = function () {
 
 getMapObject();
 
-//user location = long-min + Lat-min
-//user destination = long-max + lat-max
+//get user location and destination
+
+//set user location as long-min + Lat-min
+
+//set user destinationas long-max + lat-max
+
+//get list of locations 
+
+//display only user selected locations
+
+// attractEl.addEventListener("checkbox",getMapObject)
